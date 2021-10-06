a436768336d440a19bdfb7fb7e8541f7510fd0bc
Mohib Khan <mohibk.in@gmail.com>
2021-10-06 22:48:08 +0530

    chore: added react beautiful dnd

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Dashboard: React.FC = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="bg-green-300 h-screen">
+      <header className="bg-green-700 text-white">
+        <h1 className="text-center text-2xl py-2">Dashboard</h1>
+      </header>
     </div>
   );
 };
